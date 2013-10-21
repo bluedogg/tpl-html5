deb6048841f09e982a64de19ccafbf715653d8b4
Yu Shi <mynsa04@ya.ru>
2013-10-21 18:58:55 +0400

    Привёл в более-менее актуальный вид

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,188 @@
+'use strict';
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
 
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
 
 module.exports = function(grunt) {
-
-    var lrSnippet = require('grunt-contrib/node_modules/grunt-contrib-livereload/lib/utils').livereloadSnippet;
-    
-    var mountFolder = function(connect, dir) {
-        return connect.static(require('path').resolve(dir));
-    };
-
+    // show elapsed time at the end
+    require('time-grunt')(grunt);
     // load all grunt tasks
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
 
-    var dir = {
-            src: 'dev/',
-            dist: 'production/'
+
+    /**
+     * Пробежим по src и добавим в путь префикс dir.src/
+     * и создадим список минифицированных результирующих файлов
+     *
+     * @param {[type]} src [description]
+     * @param {[type]} pkg Если не указан, возвращает только src
+     *
+     * @return Возвращает { src: src, min: min }
+     */
+    function addPrefix(src, pkg) {
+        var i, k, min;
+
+        min = {};
+
+        if(src.length) {
+            src = src.map(function(item) {
+                return dir.src + '/' + item;
+            });
+        }
+        else {
+            for(i in src) {
+                if(src.hasOwnProperty(i)) {
+                    if(src[i].length) { // Array
+                        src[i] = src[i].map(function(item) {
+                            return dir.src + '/' + item;
+                        })
+                        if(pkg) {
+                            min[i] = dir.dist + '/' + i + '/' + pkg.name.toLowerCase() + '.min.v' + pkg.version + '.' + i; // Пока только js и css
+                        }
+                    }
+                    else { // Object
+                        min[i] = {};
+                        for(k in src[i]) {
+                            if(src[i].hasOwnProperty(k) && src[i][k].length) {
+                                src[i][k] = src[i][k].map(function(item) {
+                                    return dir.src + '/' + item;
+                                });
+                                if(pkg) {
+                                    min[i][k] = dir.dist + '/' + i + '/' + k + '.min.v' + pkg.version + '.' + i; // Пока только js и css
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return (pkg ? {
+            src: src,
+            min: min
+        } : src);
+    }
+
+
+
+
+    var tmp,
+
+        pkg = grunt.file.readJSON('package.json'),
+
+        dir = {
+            src: 'dev',
+            dist: 'production'
         },
 
+        // for jasmine testing
+        testable = [
+            'js/Tm/util.js'
+        ],
+
+        // Исходники
         src = {
+            /*
+            // Может быть массивом
             js: [
+                'vendor/modernizr/modernizr.js',
+                'vendor/jquery/jquery.js',
+
+                'js/Tm/util.js',
+                'js/app.js'
             ],
             css: [
-            ]
-        };
+                'css/basic.css',
+                'css/common.css',
+            ],
+            */
+
+            // Или объектом
+            js: {
+                early: [
+                    'js/Tm/util.js'
+                ],
+                common: [
+                    'js/app.js'
+                ],
+                vendor: [
+                    'vendor/modernizr/modernizr.js',
+                    'vendor/jquery/jquery.js'
+                ]
+            },
+            css: {
+                common: [
+                    // 'lib/bootstrap/css/bootstrap.css',
+                    'css/basic.css',
+                    'css/common.css',
+                ],
+            }
+
+        },
+
+        // Минифицированные файлы
+        min = {},
+
+        /*viewports = [
+            '1920x1080', // large desktop screen
+            '1280x1024', // standart desktop screen
+            '1024x768',  // minimum desktop screen
+            '768x1024',  // iPad
+            '640x960',   // iPhone
+            '320x240'    // shitty phone
+        ];*/
+
+        viewports = ['320x480','480x320','384x640','640x384','602x963','963x602','600x960','960x600','800x1280','1280x800','768x1024','1024x768'];
 
 
-    src.js = src.js.map(function(item) {
-        return dir.src + item;
-    });
-    src.css = src.css.map(function(item) {
-        return dir.src + item;
-    });
+
+    tmp = addPrefix(src, pkg);
+    src = tmp.src;
+    min = tmp.min;
+
+    testable = addPrefix(testable);
+
 
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+
+        pkg: pkg,
         meta: {
-        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n'
+            banner: '' +
+                    '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+                    '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                    '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+                    '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+                    ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n'
         },
 
+
         dir: dir,
-        min_file: {
-            js: '<%= dir.dist %>js/all.min.js',
-            css: '<%= dir.dist %>css/all.min.css'
-        },
+        min_files: min,
 
         /****************************************************/
 
         watch: {
             livereload: {
+                options: {
+                    livereload: LIVERELOAD_PORT
+                },
                 files: [
-                    '<%= dir.src %>*.html',
-                    '{.tmp,<%= dir.src %>}css/{,*/}*.css',
-                    '{.tmp,<%= dir.src %>}js/{,*/}*.js',
-                    '<%= dir.src %>i/{,*/}*.{png,jpg,jpeg,webp}'
-                ],
-                tasks: [ 'livereload' ]
+                    '<%= dir.src %>/*.html',
+                    '{.tmp,<%= dir.src %>}/css/{,*/}*.css',
+                    '{.tmp,<%= dir.src %>}/js/{,*/}*.js',
+                    '<%= dir.src %>/i/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+                // tasks: [ 'livereload' ]
             }
         },
         connect: {
@@ -67,6 +190,7 @@ module.exports = function(grunt) {
                 port: 9000,
                 // change this to '0.0.0.0' to access the server from outside
                 hostname: 'localhost'
+                // hostname: '0.0.0.0'
             },
             livereload: {
                 options: {
@@ -74,7 +198,7 @@ module.exports = function(grunt) {
                         return [
                             lrSnippet,
                             mountFolder(connect, '.tmp'),
-                            mountFolder(connect, 'dev')
+                            mountFolder(connect, dir.src)
                         ];
                     }
                 }
@@ -93,7 +217,7 @@ module.exports = function(grunt) {
                 options: {
                     middleware: function(connect) {
                         return [
-                            mountFolder(connect, 'production')
+                            mountFolder(connect, dir.dist)
                         ];
                     }
                 }
@@ -112,97 +236,139 @@ module.exports = function(grunt) {
 
         /****************************************************/
 
+        jasmine: {
+            src: testable,
+            options: {
+                specs: 'spec/**/*.js',
+                vendor: [
+                  '<%= dir.src %>/vendor/jquery/jquery.js'
+                ]
+            }
+        },
 
         jshint: {
             options: {
                 jshintrc: '.jshintrc'
             },
-            grunt: [ 'Gruntfile.js' ],
-            src: [ src.js ]
+            grunt: [
+                'Gruntfile.js'
+            ],
+            src: [
+                src.js
+            ]
         },
 
         uglify: {
-            options: {
+            /*options: {
                 mangle: true,
                 // beautify: true,
                 banner: '<%= meta.banner %>'
             },
             main: {
                 files: {
-                    '<%= min_file.js %>': src.js
+                    '<%= min_files.js %>': src.js
+                }
+            }*/
+
+            early: {
+                options: {
+                    mangle: true,
+                    beautify: false
+                },
+                files: {
+                    '<%= min_files.js.early %>': src.js.early
+                }
+            },
+            common: {
+                options: {
+                    mangle: true,
+                    beautify: false
+                },
+                files: {
+                    '<%= min_files.js.common %>': src.js.common
+                }
+            },
+            vendor: {
+                options: {
+                    mangle: true,
+                    beautify: false
+                },
+                files: {
+                    '<%= min_files.js.vendor %>': src.js.vendor
                 }
             }
         },
 
         cssmin: {
-            main: {
+            /*main: {
                 files: {
-                    '<%= min_file.css %>': src.css
+                    '<%= min_files.css %>': src.css
                 },
                 options: {
                     banner: '<%= meta.banner %>'
+                }
+            }*/
+
+            common: {
+                options: {
+                    banner: '<%= meta.banner %>'
+                },
+                files: {
+                    '<%= min_files.css.common %>': src.css.common
                 }
             }
         },
 
 
-
-        /*copy: {
-            jsall: {
-                files: [
-                    { expand: true, cwd: '<%= dir.dist %>', src: [ 'f/proaudio/mpc-all.min.js' ], dest: '/Users/ys/htdocs/mpc/cms/' }
-                ]
-            },
-            cssextjs: {
-                files: [
-                    { expand: true, cwd: '<%= dir.dist %>', src: [ 'f/proaudio/extjs.min.css' ], dest: '/Users/ys/htdocs/mpc/cms/' }
-                ]
-            },
-            csstriple: {
-                files: [
-                    { expand: true, cwd: '<%= dir.dist %>', src: [ 'f/proaudio/proaudio.min.css' ], dest: '/Users/ys/htdocs/mpc/cms/' },
-                    { expand: true, cwd: '<%= dir.dist %>', src: [ 'f/videogadget/videogadget.min.css' ], dest: '/Users/ys/htdocs/mpc/cms/' },
-                    { expand: true, cwd: '<%= dir.dist %>', src: [ 'f/mpcclub/mpcclub.min.css' ], dest: '/Users/ys/htdocs/mpc/cms/' },
-                    { expand: true, cwd: '<%= dir.dist %>', src: [ 'f/discount/discount.min.css' ], dest: '/Users/ys/htdocs/mpc/cms/' }
-                ]
-            }
-        },*/
-
-
-
-        /*secret: grunt.file.readJSON('secret.json'),
-        sftp: {
-            options: {
-                // privateKey: grunt.file.load("id_rsa"),
-                srcBasePath: 'production/',
-                path: './www/cms.mpc.ru/www/',
-                host: '<%= secret.host %>',
-                port: '<%= secret.port %>',
-                username: '<%= secret.username %>',
-                password: '<%= secret.password %>'
-            },
-            js: {
+        targethtml: {
+            dist: {
+                options: {
+                    curlyTags: {
+                        version: '<%= pkg.version %>'
+                    }
+                },
                 files: {
-                    './': '<%= min_file.js %>'
-                }
-            },
-            css: {
-                files: {
-                    './': '<%= min_file.css %>'
+                    '<%= dir.dist %>/index.html': '<%= dir.src %>/index.html',
                 }
             }
-        },*/
+        },
+
+
+        autoshot: {
+            /*default_options: {
+                options: {
+                    // necessary config
+                    path: 'screenshots/',
+                    filename: '',
+                    type: 'PNG',
+                    // optional config, must set either remote or local
+                    remote: 'http://localhost:<%= connect.options.port %>',
+                    // local: { path: 'dev/index.html', port: '9001' },
+                    viewport: viewports
+                },
+            },*/
+
+            index: {
+                options: {
+                    path: 'screenshots/',
+                    filename: 'index',
+                    type: 'png',
+                    remote: 'http://localhost:<%= connect.options.port %>',
+                    viewport: viewports
+                }
+            }
+        },
+
+
 
 
         empty: {}
 
     });
 
+    /****************************************************/
 
-
-
-
-    grunt.registerTask('nothing', [
+    grunt.registerTask('default', [
     ]);
 
     grunt.renameTask('regarde', 'watch');
@@ -223,19 +389,21 @@ module.exports = function(grunt) {
         ]);
     });
 
+    /****************************************************/
+
     grunt.registerTask('js', [
-        'jshint:grunt',
-        'jshint:src',
-        'uglify:main'
+        // 'jshint:grunt',
+        // 'jshint:src',
+        // 'uglify:main'
     ]);
 
     grunt.registerTask('css', [
-        'cssmin:main'
+        // 'cssmin:main'
     ]);
 
     grunt.registerTask('build', [
-        'js',
-        'css'
+        // 'js',
+        // 'css'
     ]);
 
 };
---
+++ dev/js/Tm/util.js
@@ -0,0 +1,374 @@
+var Tm = Tm || {};
+
+/**
+ *  No jQuery using
+ *
+ * @param  {[type]} global [description]
+ * @return {[type]}        [description]
+ */
+Tm.util = (function(global) {
+
+    // Для Safari
+    global.URL = global.URL || global.webkitURL;
+
+
+
+    /**
+     * Alias of Tm.util.namespace()
+     *
+     * @return {[type]} [description]
+     */
+    global.Tm.ns = function() {
+
+        return global.Tm.util.namespace.apply(global, arguments);
+    }
+
+
+    /**
+     * Detect browser & os
+     *
+     * @return {[type]} [description]
+     */
+    var browser = (function() {
+        var ua = navigator.userAgent.toLowerCase(),
+            engine = ua.match(/(webkit|gecko|presto|trident)/ig),
+            vendor = ua.match(/(firefox|safari|chrome|opera|msie)/ig),
+            os = ua.match(/(os\sx|windows|linux)/ig),
+            mobile = ua.match(/(mobile)/ig);
+
+        engine = engine[0].toLowerCase() || 'unknown';
+        vendor = vendor[0].toLowerCase() || 'unknown';
+        os = os[0].replace(' ', '').toLowerCase() || 'unknown';
+        mobile = (mobile ? true : false);
+
+        return {
+            engine: engine, // webkit | gecko | presto | trident
+            vendor: vendor, // firefox | safari | chrome | opera | msie
+            os: os,         // windows | linux | osx
+            mobile: mobile  // true | false
+        }
+    })();
+
+
+    /**
+     * Включает загрузку "ленивых" картинок (у которых @src="/i/px.gif" @data-src="/f/image-url.png")
+     *
+     * body/@data-lazyimageload включает автозагрузку, без принудительного вызова из js
+     *
+     * Нужен img/@data-ratio="[ w / h ]", для корректного выставления высоты до загрузки
+     *
+     * @return {[type]} [description]
+     */
+    var lazyImageLoad = function() {
+        var imgs, imgi, len, src, img, ratio, width_real, height_real, width_new, height_new,
+            imgsPending = [];
+
+        function onloadHandler() {
+            var i = this.getAttribute('data-i');
+
+            // console.log(this, i);
+
+            imgs[i].src = this.src;
+            imgs[i].style.width = 'auto';
+            imgs[i].style.height = 'auto';
+        }
+
+        imgs = global.document.getElementsByTagName('img');
+        len = imgs.length;
+
+        if(len) {
+            for(var i = 0; i < len; i++) {
+                imgi = imgs[i];
+                src = imgi.getAttribute('data-src');
+                ratio = imgi.getAttribute('data-ratio');
+                // Соотношение есть
+                if(ratio) {
+                    width_real = imgi.getAttribute('width');
+                    height_real = imgi.getAttribute('height');
+                    width_new = imgi.parentNode.offsetWidth;
+                    // Ширина меньше контейнера, ок, выставим реальное и не паримся
+                    if(width_real <= width_new) {
+                        width_new = width_real;
+                        height_new = height_real;
+                    }
+                    // Не, картинка шире, надо посчитать высоту
+                    else {
+                        height_new = Math.round(width_new / ratio);
+                    }
+                }
+                // console.log(ratio, imgi.parentNode, width_real, 'x', height_real, ' | ', width_new, 'x', height_new);
+                if(src) {
+                    img = new Image();
+                    img.onload = onloadHandler;
+                    img.setAttribute('data-i', i);
+
+                    imgi.style.width = width_new + 'px';
+                    imgi.style.height = height_new + 'px';
+
+                    // img.src = src;
+                    imgsPending.push({
+                        img: img,
+                        src: src
+                    });
+                    // imgi.src = src;
+                }
+            }
+            global.onload = function() {
+                var len = imgsPending.length;
+
+                // console.log(imgsPending);
+                if(len) {
+                    for(var i = 0; i < len; i++) {
+                        imgsPending[i].img.src = imgsPending[i].src;
+                    }
+                }
+            }
+        }
+    };
+
+    // check lazy image load option
+    if(global.document.body && global.document.body.getAttribute('data-lazyimageload') !== null) {
+        lazyImageLoad();
+    }
+
+
+
+
+    return {
+
+
+        browser: function() {
+
+            return browser;
+        },
+
+
+        /**
+         * Create namespaces
+         *
+         * Usage:
+         *     Tm.util.namespace('parent.child.subchild');
+         *     Tm.util.namespace('parent.child.subchild', 'ns1.ns2.ns3');
+         *
+         * @return {[type]} [description]
+         */
+        namespace: function() {
+            var i, j, d,
+                arg = arguments,
+                o = null;
+
+            for(i = 0; i < arg.length; i = i + 1) {
+                d = arg[i].split('.');
+                o = window;
+                for(j = 0; j < d.length; j = j + 1) {
+                    o[ d[j] ] = o[ d[j] ] || {};
+                    o = o[ d[j] ];
+                }
+            }
+
+            return o;
+        },
+
+
+        /**
+         * Strip tags from string
+         *
+         * @param  {string} str [description]
+         * @return {[type]}     [description]
+         */
+        stripTags: function(str) {
+            return !str ? str : String(str).replace(/<\/?[^>]+>/gi, '');
+        },
+
+
+        /**
+         * [trim description]
+         *
+         * @param  {[type]} str [description]
+         * @return {[type]}     [description]
+         */
+        trim: function(str) {
+            return str ? str.replace(/^[\x09\x0a\x0b\x0c\x0d\x20\xa0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f\u3000]+|[\x09\x0a\x0b\x0c\x0d\x20\xa0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f\u3000]+$/g, '') : str;
+        },
+
+
+        /**
+         * [nl2br description]
+         *
+         * @param  {[type]} str [description]
+         * @return {[type]}     [description]
+         */
+        nl2br: function(str) {
+            return str ? str.replace(/\r?\n/g, '<br/>') : str;
+        },
+
+
+        /**
+         * [br2nl description]
+         *
+         * @param  {[type]} str [description]
+         * @return {[type]}     [description]
+         */
+        br2nl: function(str) {
+            return str ? str.replace(/<br(\s*\/?)>/g, '\r\n') : str;
+        },
+
+
+        /**
+         * Возвращает название месяца
+         *
+         * @param  {number} monthNum порядковый номер месяца ( 1 .. 12 )
+         * @param  {number} type тип названия ( full | full2 )
+         * @param  {number} lang на каком языке возвращать ( ru )
+         * @return {string} если не нашли, то -1
+         */
+        getMonthName: function(monthNum, type, lang) {
+            var MONTH_NAMES = {
+                ru: {
+                    full: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+                    full2: ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+                }
+            };
+
+            type = type || 'full';
+            lang = lang || 'ru';
+
+            if(monthNum >= 1 && monthNum <= 12) {
+                return MONTH_NAMES[lang][type][monthNum - 1];
+            }
+
+            return -1;
+        },
+
+
+        /**
+         * [parseSize description]
+         *
+         * @param  {[type]} size [description]
+         * @return {[type]}      [description]
+         */
+        getHumanSize: function(size, type, lang) {
+            var suffix = {
+                    ru: {
+                        short: ['байт', 'КБ', 'МБ', 'ГБ', 'ТБ', 'ПБ']
+                    },
+                    en: {
+                        short: ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB']
+                    }
+                },
+                c = 0;
+
+            type = type || 'short';
+            lang = lang || 'ru';
+
+            while(size >= 1024) {
+                size = size / 1024;
+                c++;
+            }
+
+            return Math.round(size * 10) / 10 + ' ' + suffix[c];
+        },
+
+
+        /**
+         * форматирует вывод числа, аналог number_format() в PHP
+         *
+         * @param  {[type]} number        [description]
+         * @param  {[type]} decimals      [description]
+         * @param  {[type]} dec_point     [description]
+         * @param  {[type]} thousands_sep [description]
+         * @return {[type]}               [description]
+         */
+        numberFormat: function(number, decimals, dec_point, thousands_sep) {
+            var i, z, temp, sign, integer, fractional,
+                exponent = '',
+                numberstr = number.toString(),
+                eindex = numberstr.indexOf('e');
+
+            if(number) {
+                thousands_sep || (thousands_sep = ' ');
+
+                if(eindex > -1) {
+                    exponent = numberstr.substring(eindex);
+                    number = parseFloat(numberstr.substring(0, eindex));
+                }
+
+                decimals || (decimals = 0);
+                if(decimals !== null) {
+                    temp = Math.pow(10, decimals);
+                    number = Math.round(number * temp) / temp;
+                }
+                sign = (number < 0 ? '−' : '');
+                integer = (number > 0 ? Math.floor(number) : Math.abs(Math.ceil(number))).toString();
+
+                fractional = number.toString().substring(integer.length + sign.length);
+                dec_point = (dec_point !== null ? dec_point : '.');
+                fractional = (decimals !== null && decimals > 0 || fractional.length > 1 ? dec_point + fractional.substring(1) : '');
+                if(decimals !== null && decimals > 0) {
+                    for(i = fractional.length - 1, z = decimals; i < z; ++i) {
+                        fractional += '0';
+                    }
+                }
+
+                thousands_sep = ((thousands_sep != dec_point || fractional.length === 0) ? thousands_sep : null);
+                if(thousands_sep !== null && thousands_sep !== '') {
+                    for (i = integer.length - 3; i > 0; i -= 3) {
+                        integer = integer.substring(0 , i) + thousands_sep + integer.substring(i);
+                    }
+                }
+
+                result = sign + integer + fractional + exponent;
+            }
+
+            return result;
+        },
+
+
+        /**
+         * Вешает варнинг на обновление, закрытие или уход со страницы
+         *
+         * @param  {[type]} msg если сообщение пустое, то хэндлер прибивается
+         * @return {[type]}     [description]
+         */
+        reloadWarning: function(msg) {
+            if(msg) {
+                global.onbeforeunload = function() {
+                    return msg;
+                }
+            }
+            else {
+                global.onbeforeunload = null;
+            }
+        },
+
+
+        /**
+         * Checking type of JS item
+         * http://tech.yandex.ru/events/yac/2013/talks/1116/
+         *
+         * @param  {[type]} o [description]
+         * @return {[type]}   [description]
+         */
+        typeof: function(o) {
+            var _match, _type;
+
+            if(o && o.nodeType === 1) {
+                _type = 'element';
+            }
+
+            _match = Object.prototype.toString.call(o).match(/\[object (.*?)\]/);
+            _type = _match[1].toLowerCase();
+
+            if(_type === 'number' && isNaN(o)) {
+                _type = 'nan';
+            }
+
+            return _type;
+        },
+
+
+        lazyImageLoad: lazyImageLoad
+
+    };
+
+})(window, undefined);
